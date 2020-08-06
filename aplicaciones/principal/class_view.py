d9d0bca6b6a4fb83dfee404bd4bcf9e0ac8f7940
fjuribe <fjuribeui@gmail.com>
2020-08-06 00:46:34 -0400

    usando Class CRUD

---
+++ aplicaciones/principal/class_view.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render,redirect
+from django.views.generic import CreateView,DeleteView,ListView,UpdateView #view
+from .form import PersonaForm
+from .models import Persona
+
+
+
+
+class PersonaList(ListView):
+	model=Persona
+	template_name='index.html'
+
+	def get_queryset(self):
+		return self.model.objects.all()[:2]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+class View():
+	dispatch:         verifica el metodo de la solicitud http
+	http_not_allowed: 
+
+	def get_queryset(self):
+		return self.model.objects.all()
+
+	def get_templates_names():
+		return self.template_name
+
+	def get(self,request,*args,**kwargs):
+		return render(request,self_templates_names(),self.get_queryset())
+
+
+
+
+class ListView(View):
+	model         =Persona
+	template_name=='index.html'
+
+	def dispatch()
+
+	def get_contex_data(self):
+		context={}
+		context['queryset']=self.get_queryset()
+		return context
+
+	def get_queryset(self):
+		return self.model.objects.all()
+
+	def get_templates_names():
+		return self.template_name
+
+	def get(self,request,*args,**kwargs):
+		return render(request,self_templates_names(),self.get_queryset())
+"""
